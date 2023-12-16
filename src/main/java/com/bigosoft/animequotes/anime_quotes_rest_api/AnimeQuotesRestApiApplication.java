79a86a98577e3064094da2c08001cda08318043d
muhannad <azxshatatt171@gmail.com>
2023-12-16 20:08:58 +0300

    top_project

---
+++ src/main/java/com/bigosoft/animequotes/anime_quotes_rest_api/AnimeQuotesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.bigosoft.animequotes.anime_quotes_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeQuotesRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeQuotesRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigosoft/animequotes/anime_quotes_rest_api/AnimeQuotesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigosoft.animequotes.anime_quotes_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeQuotesRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
